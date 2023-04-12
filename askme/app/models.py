7678341c4f22e935312e154aea7908f2828f1b73
Butters7 <egorka.davydov.1980@mail.ru>
2023-04-13 00:31:49 +0300

    updates

--- askme/app/models.py
+++ askme/app/models.py
@@ -1,7 +1,33 @@
 QUESTIONS = [
     {
         'id': i,
+        'user_name': f'user {i}',
         'title': f'Question {i}',
         'text': f'Text {i}',
     } for i in range(3)
+]
+
+
+ANSWERS = [
+    {
+        'id': i,
+        'user_name': f'user {i}',
+        'text': f'blablabla {i}',
+    } for i in range(3)
+]
+
+
+BEST_MEMBERS = [
+    {
+        'id': i,
+        'user_name': f'user {i}',
+    } for i in range(5)
+]
+
+
+TAGS = [
+    {'tag_name': 'python', 'id': 0, 'color': 'magenta'},
+    {'tag_name': 'technopark', 'id': 1, 'color': 'green'},
+    {'tag_name': 'mySQL', 'id': 2, 'color': 'blue'},
+    {'tag_name': 'blender', 'id': 3, 'color': 'red'},
 ]
--- askme/app/views.py
+++ askme/app/views.py
@@ -3,33 +3,39 @@ from django.shortcuts import render
 
 # Create your views here.
 def ask(request):
-    return render(request, 'ask.html')
+    context = {'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
+    return render(request, 'ask.html', context)
 
 
 def base(request):
-    return render(request, 'base.html')
+    context = {'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
+    return render(request, 'base.html', context)
 
 
 def index(request):
-    context = {'questions': models.QUESTIONS}
+    context = {'questions': models.QUESTIONS, 'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
     return render(request, 'index.html', context)
 
 
 def login(request):
-    return render(request, 'login.html')
+    context = {'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
+    return render(request, 'login.html', context)
 
 
 def question(request, question_id):
-    context = {'question': models.QUESTIONS[question_id]}
+    context = {'question': models.QUESTIONS[question_id], 'answers': models.ANSWERS, 'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
     return render(request, 'question.html', context)
 
 
 def settings(request):
-    return render(request, 'settings.html')
+    context = {'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
+    return render(request, 'settings.html', context)
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    context = {'tags': models.TAGS, 'bm': models.BEST_MEMBERS}
+    return render(request, 'signup.html', context)
 
 def tag(request):
+    # context = {'tag': models.TAGS[tag_name]}
     return render(request, 'tag.html')
