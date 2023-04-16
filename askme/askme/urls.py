9603ec07bb6b398c897de59b23c88acf4d1cf403
Butters7 <egorka.davydov.1980@mail.ru>
2023-04-17 02:23:31 +0300

    change path on re_path in root urls

--- askme/askme/urls.py
+++ askme/askme/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from app import views
 
 urlpatterns = [
-    path('', include('app.urls')),
+    re_path('^', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
